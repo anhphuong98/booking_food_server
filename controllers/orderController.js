896fd9c71105511f77b767e1ac9859c2ae3f9ec5
nganluu <nganltt712@gmail.com>
2019-12-21 23:14:03 +0700

    update

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,12 +2,12 @@ db = require('../models');
 
 //get all order 
 const getAllOrder = (req, res) => {
-        db.order.findAll().then(function(result){
-            res.status(200).json({
-                success: true,
-                data: result
-            })
+    db.order.findAll().then(function (result) {
+        res.status(200).json({
+            success: true,
+            data: result
         })
+    })
 }
 //get order with shipper id
 const getOrderShipper = (req, res) => {
@@ -15,13 +15,46 @@ const getOrderShipper = (req, res) => {
         where: {
             shipper_id: req.user.id
         }
-    }).then(function(result){
-
+    }).then(function (result) {
+        if (req.user.id == req.params.id) {
+            
+            if (result) {
+                res.status(200).json({
+                    success: true,
+                    data: result
+                })
+            }
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "cannot get order by admin id"
+            })
+        }
     })
 }
+const getOrderShipperTest = (req, res) => {
+    db.findAll({
+       include: [{
 
+       }]
+    })
+}
+const getDetailbyOrderId = (req, res) => {
+    db.order_detail.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (result) {
+        res.status(200).json({
+            success: true,
+            data: result
+        })
+    })
+}
 const orderController = {};
 orderController.getAllOrder = getAllOrder
 orderController.getOrderShipper = getOrderShipper;
+orderController.getOrderShipperTest = getOrderShipperTest;
+orderController.getDetailbyOrderId = getDetailbyOrderId;
 
 module.exports = orderController;
